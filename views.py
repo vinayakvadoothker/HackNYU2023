bbf7dc6b709288a9ce9cfe646e2ac60760ba7886
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2023-02-19 07:29:07 -0500

    update

--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from data import get_url
 
 views = Blueprint(__name__, 'views')
 
+number = 1234567890
+
 @views.route('/')
 def login():
     return render_template('pages-login.html')
@@ -11,45 +13,45 @@ def login():
 @views.route('/home.html', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+        global number
         number = request.form['phone-number']
         introText(number)
         
-        return render_template('home.html')
-    else:
-        return render_template('pages-login.html')
+    return render_template('home.html', userNum=number)
+    
 
 
 @views.route('/invest.html', methods=['GET', 'POST'])
 def home2():
-    if request.method == 'POST':
+    if request.method == 'POST': # needs to be added in template not invest
             number = request.form['searchInput']
             name = searchInput
             url = get_url(searchInput)
             twitter_name = searchInput.replace(' ', '')
             ticker = searchInput[:4]
             return render_template('template.html',name = name, url = url, twitter_name = twitter_name, ticker = ticker) 
-        else:
-            return render_template('invest.html')
+    else:
+            return render_template('invest.html', userNum=number)
 
 @views.route('/about_us.html')
 def about():
-    return render_template('about_us.html')
+    return render_template('about_us.html', userNum=number)
 
 @views.route('/charli.html')
 def charli():
-    return render_template('charli.html')
+    return render_template('charli.html', userNum=number)
 
 @views.route('/contact_thank_you.html')
 def thank_you():
-    return render_template('contact_thank_you.html')
+    return render_template('contact_thank_you.html', userNum=number)
 
 @views.route('/contact.html')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', userNum=number)
 
 @views.route('/deposit.html')
 def deposit():
-    return render_template('deposit.html')
+    return render_template('deposit.html', userNum=number)
 
 @views.route('index.html')
 def index():
@@ -57,15 +59,15 @@ def index():
 
 @views.route('/justin_bieber.html')
 def justin():
-    return render_template('justin_bieber.html')
+    return render_template('justin_bieber.html', userNum=number)
 
 @views.route('/logan_paul.html')
 def logan():
-    return render_template('logan_paul.html')
+    return render_template('logan_paul.html', userNum=number)
 
 @views.route('/mr_beast.html')
 def beast():
-    return render_template('mr_beast.html')
+    return render_template('mr_beast.html', userNum=number)
 
 @views.route('/pages-login.html')
 def plogin():
