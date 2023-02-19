d9ae82a19258e08138d90a42b676a457ae60bc54
vinayakvadoothker <74890320+vinayakvadoothker@users.noreply.github.com>
2023-02-18 21:55:37 -0500

    app.py

--- register.py
+++ register.py
@@ -1,9 +1,3 @@
-import hashlib
-
 def register_user(name, email, username, password):
-    # Hash the password using SHA-256 algorithm
-    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-
-    # Store user data in a text file
     with open('users.txt', 'a') as file:
-        file.write(f'{name},{email},{username},{hashed_password}\n')
+        file.write(f"{name},{email},{username},{password}\n")
